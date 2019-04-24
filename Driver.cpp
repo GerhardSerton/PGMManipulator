7c37bac0ad70cd0900bcf0bf8e74e9f72cac3291
Gerhard Serton <gerhardserton@gmail.com>
2019-04-24 16:29:01 +0200

    Created basic framework for Driver class

---
+++ Driver.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include "Image.h"
+
+using namespace std;
+int main (int argc, char*argv[])
+{
+	string command = argv[1];
+
+	if (command == "-a")
+	{
+		string i1 = argv[2];
+		string i2 = argv[3];
+		Image image1(i1);
+		Image image2(i2);
+		//image1 = image1 + image2;
+	}
+	else if (command == "-s")
+	{
+
+		string i1 = argv[2];
+		string i2 = argv[3];
+		Image image1(i1);
+		Image image2(i2);
+		//image1 = image1 - image2;
+	}
+	else if (command == "-i")
+	{
+		string i1 = argv[2];
+		Image image1(i1);
+		//image1 = !image1;
+	}
+	else if (command == "-l")
+	{
+
+		string i1 = argv[2];
+		string i2 = argv[3];
+		Image image1(i1);
+		Image image2(i2);
+		//image1 = image1/image2;
+	}
+	else if (command == "-t")
+	{
+
+		string i1 = argv[2];
+		int i2 = stoi(argv[3]);
+		Image image1(i1);
+		//image1 = image1 * i2;
+	}
+	//Test command, just loads the file in
+	else
+	{
+		Image image(command);
+	}
+}
